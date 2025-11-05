b8ffb78be4209f338093776212668fce3c15ebed
cemile871 <250541099@firat.edu.tr>
2025-11-05 21:46:53 +0300

    Update GeometrikHesap.java

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -1,1 +1,46 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class GeometrikHesap {
+    static void main(String[] args) {
+
+     final double PI = 3.14159;
+
+     //Kullanıcıdan bilgi almak için scanner nesnesi oluşturuluyor
+     Scanner scanner = new Scanner(System.in);
+
+     System.out.println("=== GEOMETRİK HESAPLAYICI ===");
+     System.out.print("Dairenin yaricapini girin (cm):");
+
+     double r = scanner.nextDouble();
+
+     //Scanner nesnesini kapat
+     scanner.close();
+
+     //Hesaplamalar
+     double daireAlani = PI * Math.pow(r, 2);
+
+     double daireCevresi = 2 * PI * r ;
+
+     double daireCapi = 2 * r ;
+
+     double kureHacmi = (4.0 / 3.0) * PI * Math.pow(r,3);
+
+     double kureYuzeyAlani = 4 * PI * Math.pow(r, 2);
+
+     //Bilgileri ekrana düzenli yazdırma aşaması
+     System.out.println("\nSONUCLAR:");
+     System.out.println("------------");
+
+
+        System.out.printf("%-20s : %8.2f %s\n", "Daire Alani", daireAlani, "cm^2");
+        System.out.printf("%-20s : %8.2f %s\n", "Daire Cevresi", daireCevresi, "cm");
+        System.out.printf("%-20s : %8.2f %s\n", "Daire Capi", daireCapi, "cm");
+        System.out.printf("%-20s : %8.2f %s\n", "Kure Hacmi", kureHacmi, "cm^3");
+        System.out.printf("%-20s : %8.2f %s\n", "Kure Yuzey Alani", kureYuzeyAlani, "cm^2");
+
+
+    }
+
+}
 
