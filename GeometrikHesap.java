be1d0452c546d90fd5a3b9000002dd71909633f7
cemile871 <250541099@firat.edu.tr>
2025-11-05 22:35:41 +0300

    Update GeometrikHesap.java

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class GeometrikHesap {
-    static void main(String[] args) {
+   public static void main(String[] args) {
 
      final double PI = 3.14159;
 
